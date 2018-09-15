8ea803419d142b834c4f0a9150dd01c870821210
jfarr133 <jakobpaul13@gmail.com>
2018-09-15 16:06:18 +1000

    Made slight changes to get network connected

--- networkTest/networkTestForm2/networkTestForm2/Program.cs
+++ networkTest/networkTestForm2/networkTestForm2/Program.cs
@@ -7,13 +7,13 @@ using System.Windows.Forms;
 namespace networkTestForm2
 {
     static class Program
-    {
+    { //test
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
         [STAThread]
         static void Main()
-        {
+        { 
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
